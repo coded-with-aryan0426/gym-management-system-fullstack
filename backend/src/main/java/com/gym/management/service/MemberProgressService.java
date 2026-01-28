42cb739788a480bab3bda61e4c25b411cb88b7f4
coded-with-aryan0426 <aryansuthar194@gmail.com>
2026-01-28 16:34:17 +0530

    Auto-sync after agent response

--- backend/src/main/java/com/gym/management/service/MemberProgressService.java
+++ backend/src/main/java/com/gym/management/service/MemberProgressService.java
@@ -4,6 +4,7 @@ import com.gym.management.dto.progress.*;
 import com.gym.management.model.*;
 import com.gym.management.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
